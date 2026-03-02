c6b923d6175daf080956e1d48f811b339ace3dd4
Matt Coul <matthew.couldery@gmail.com>
2026-03-02 17:03:59 +0000

    updating for task display

--- app/models.py
+++ app/models.py
@@ -8,6 +8,7 @@ class Task(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     name: so.Mapped[str] = so.mapped_column(sa.String(256), index=True, nullable=False)
     description: so.Mapped[str] = so.mapped_column(sa.String(512))
+    type: so.Mapped[str] = so.mapped_column(sa.String(265), default="Custom")
     module: so.Mapped[str] = so.mapped_column(sa.String(256))
     points: so.Mapped[int] = so.mapped_column(nullable=False)
     due_date: so.Mapped[str] = so.mapped_column(sa.DATE, nullable=False, default=lambda: datetime.now(timezone.utc))
--- app/routes.py
+++ app/routes.py
@@ -3,8 +3,13 @@ from app import app
 from app import db
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy import func
-
+from app.models import Task
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+
+def task_display():
+    query = Task
+    task_type = request.args.get("task_type")
+    return render_template('task_display', task_type=task_type, )
