1f15b18e2bb7be0d26fa559718ef06928d1d162e
Matt Coul <matthew.couldery@gmail.com>
2026-03-02 13:56:36 +0000

    adding routes

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
 from flask import render_template, redirect, url_for, flash, request
 from app import app
 from app import db
-from app.forms import EventForm
-from app.models import Event
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy import func
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
