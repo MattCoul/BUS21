83489e392bd35a52fe1a4ecd459f9ad0f155686e
Matt Coul <matthew.couldery@gmail.com>
2026-03-02 15:59:57 +0000

    added base database models

--- app/models.py
+++ app/models.py
@@ -2,3 +2,12 @@ from app import db
 import sqlalchemy.orm as so
 import sqlalchemy as sa
 from datetime import datetime, timezone
+
+# The base class database model
+class Task(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    name: so.Mapped[str] = so.mapped_column(sa.String(256), index=True, nullable=False)
+    description: so.Mapped[str] = so.mapped_column(sa.String(512))
+    module: so.Mapped[str] = so.mapped_column(sa.String(256))
+    points: so.Mapped[int] = so.mapped_column(nullable=False)
+    due_date: so.Mapped[str] = so.mapped_column(sa.DATE, nullable=False, default=lambda: datetime.now(timezone.utc))
