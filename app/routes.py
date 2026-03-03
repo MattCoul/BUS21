7a2b9e088cf502b8a77d9ad696c5ea1f1e9db4f9
Matt Coul <matthew.couldery@gmail.com>
2026-03-03 11:24:36 +0000

    added task display logic

--- app/routes.py
+++ app/routes.py
@@ -12,10 +12,35 @@ def index():
 
 @app.route('/task_display')
 def task_display():
-    query = Task
+    query = Task.query
+
+    # Type of tasks display logic
     task_type = request.args.get("task_type")
+    if task_type == "exam":
+        query = query.filter(Task.task == "exam")
+    elif task_type == "groupProject":
+        query = query.filter(Task.task == "groupProject")
+    elif task_type == "coursework":
+        query = query.filter(Task.task == "coursework")
+    elif task_type == "customTasks":
+        query = query.filter(Task.task == "customTasks")
+
+    # Order of displayed tasks logic
     order = request.args.get("order")
-    tasks = []
+    if order == "name":
+        query = query.order_by(Task.name.desc())
+    elif order == "type":
+        query = query.order_by(Task.type.desc())
+    elif order == "module":
+        query = query.order_by(Task.module.desc())
+    elif order == "points":
+        query = query.order_by(Task.points.desc())
+    elif order == "due_date":
+        query = query.order_by(Task.due_date.desc())
+
+    # tasks =  query.all() # To be used one task creation set up
+    tasks = [] # Dummy until task creation is set up
+
     return render_template('task_display.html', task_type=task_type, order=order, tasks=tasks, now=datetime.now())
 
 @app.route('/task_creation')
