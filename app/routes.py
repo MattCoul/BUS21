12d00f4fc5dad6423b2bf81078faf6169f9a1739
T Jack <tjeidanjack@gmail.com>
2026-04-29 01:28:28 +0100

    last minute fixes

--- app/routes.py
+++ app/routes.py
@@ -174,12 +174,8 @@ def view_points():
         ).all()
         points_total = points_total[0][0]
 
-    if Goal.query.scalar():
-        goal = Goal.query.order_by(Goal.id.desc()).all()
-        if goal:
-            goal1 = goal[0].goal
-    else:
-        goal1 = None
+    goal = Goal.query.order_by(Goal.id.desc()).all()
+    goal1 = goal[0].goal
 
     return render_template('view_points.html', points=points, points_total=points_total, goal=goal1)
 
